73df005b249591d0abaff401db01f912cb437601
oguzhanascier <oguzhanascier20@gmail.com>
2022-12-07 13:25:30 +0300

    init

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# twitter-clone
+# nodeal
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import './assets/styles/index.css';
+
+Vue.config.productionTip = true;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    // { path: '/page', component: App },
+  ],
+});
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+  router
+}).$mount('#app');
